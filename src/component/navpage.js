4c0b32a2880a8a1c8a1b9370baf47e54065cb683
prerna <prernanikhare@gmail.com>
2023-09-22 10:52:37 +0530

    first

--- src/component/navpage.js
+++ src/component/navpage.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import Homepage from "./homepage";
+// import Homepage from "./homepage";
 import Admin from "../blogpost";
 import Blog from "../post";
 
